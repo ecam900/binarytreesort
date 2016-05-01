2277cf3cf3e16a61d38ff40392dea2a7176b7d8e
Eric Crescioni <ecam900@gmail.com>
2016-05-01 14:41:17 +0000

    add Node class

--- lib/binarytreesort.rb
+++ lib/binarytreesort.rb
@@ -1,13 +1,20 @@
+require 'node'
+
 class BTreesort
-	attr_reader :payload, :left, :right
+	attr_reader :payload, :left, :right, :sort
 
-def initialize(payload, left=nil, right=nil)
-	@payload = payload
-	@left = left
-	@right = right
+	def initialize(payload, left=nil, right=nil)
+		@payload = payload
+		@left = left
+		@right = right
+	end
+
+	def sort(array)
+
+	end
+
+
+
 end
 
-
-
-
-end
+#array = [7, 4, 9, 1, 6, 14, 10]
--- spec/binarytreesort_spec.rb
+++ spec/binarytreesort_spec.rb
@@ -1,4 +1,6 @@
 require 'spec_helper'
+require 'binarytreesort'
+require 'node'
 
 describe BTreesort, type: :model do
 	describe "::initialize" do
@@ -12,6 +14,16 @@ describe BTreesort, type: :model do
 			it "should have @left and @right be nil" do
 				tree = BTreesort.new(20)
 				expect(tree.left).to be_nil
+			end
+		end
+	end
+
+	describe "#sort" do
+		context "when given an array with unsorted numbers" do
+			it "return an array with sorted numbers" do
+      expected = [1, 4, 6, 7, 9, 10, 14]
+      array = [7, 4, 9, 1, 6, 14, 10]
+      expect(BTreesort.sort(array)).to eq expected
 			end
 		end
 	end
