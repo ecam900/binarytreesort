c7f290d0b192b05ef50505b64e8947496022fb90
Eric Crescioni <ecam900@gmail.com>
2016-05-01 23:24:24 +0000

    Trying to build tree.

--- lib/binarytreesort.rb
+++ lib/binarytreesort.rb
@@ -1,50 +1,59 @@
 class BinaryTree
-	attr_accessor :payload, :left, :right
+  attr_accessor :payload, :left, :right
 
-	def initialize(payload, left=nil, right=nil)
-		@payload = payload
-		@left = left
-		@right = right
-	end
+  def initialize(payload, left=nil, right=nil)
+    @payload = payload
+    @left = left
+    @right = right
+  end
 
 end
 
 class BTreesort
 
-	def initialize
-		@btreesort = []
-	end
+  def initialize
+    @btreesort = []
+  end
 
-	def self.sort(array)
-		tree_representation = build_tree(array)
+  def self.sort(array)
+    tree_representation = build_tree(array)
 
-		return flatten_tree(tree_representation)
-	end
+    return flatten_tree(tree_representation)
+  end
 
-	def flatten_tree(tree)
-		# build new array by flattening the tree
-		# do DFS algorigth, but modify it such that you deal with the:
-		# - left hand side
-		# - the node itself
-		# - then the right hand side
-	end
+  def flatten_tree(tree)
+    # build new array by flattening the tree
+    # do DFS algorigth, but modify it such that you deal with the:
+    # - left hand side
+    # - the node itself
+    # - then the right hand side
+  end
 
-	def build_tree(array)
-		first = array[0]
-		tree  = BinaryTree.new(first)
-		rest  = array.last(array.length - 1)
+  def build_tree(array)
+    first = array[0]
+    tree  = BinaryTree.new(first)
+    rest  = array.last(array.length - 1)
 
-		#begin
-			# 1. Loop through the rest array, for each element:
-			#    2.a) For each element find the location that each element should go in the tree.
-			#  	      to do that, keep looking through the children until you get to point where
-			# 			  if you were to insert the element, you'd be replacing a 'nil' child.
-			#    2.b) Put it at the location where it belongs, therefore modifying the tree itself.
- 			#         Build a binary tree containing the current element, two nil children and put it
- 			#         into the tree itself.
-		#end
-	
-	end
+    rest.each do |n|
+
+      if n < first
+        
+      end
+    end
+      
+      return tree unless tree.nil?
+
+    #begin
+      # 1. Loop through the rest array, for each element:
+      #    2.a) For each element find the location that each element should go in the tree.
+      #         to do that, keep looking through the children until you get to point where
+      #         if you were to insert the element, you'd be replacing a 'nil' child.
+      #    2.b) Put it at the location where it belongs, therefore modifying the tree itself.
+      #         Build a binary tree containing the current element, two nil children and put it
+      #         into the tree itself.
+    #end
+  
+  end
 
 
 
